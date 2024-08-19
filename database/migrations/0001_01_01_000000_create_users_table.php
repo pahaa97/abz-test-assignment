74578a59707f7bbc1b162ab1b8d239f54a223c41
pavelfedotov <pahaa97@gmail.com>
2024-08-19 14:43:48 +0200

    update migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('position_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone')->unique();
             $table->string('photo_url')->nullable();
             $table->timestamps();
-
-            $table->foreign('position_id')->references('id')->on('positions');
         });
     }
 
